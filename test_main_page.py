e895958b1e526e4f65cf2ef5b01cc1afc0184860
PythonStrangler <javajun90@mail.ru>
2020-01-06 15:29:23 +0300

    Реализована проверка LoginPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .Pages.main_page import MainPage
+from .Pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -13,3 +14,11 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+    # pytest -v --tb=line --language=en test_main_page.py
